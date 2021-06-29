aa5688c3823b52ed4905736759d375a0af42ec98
Burda Kacper <burda.kacper.kontakt@gmail.com>
2021-06-29 20:28:52 +0200

    Modified login and base, added scripts

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+    public function index(){
+        return $this->render("homepage/homepage.html.twig");
+    }
+}
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,14 +9,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('homepage');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -26,11 +23,7 @@ class LoginController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
